39762385d40e3f47e647e7fcb6dc5190fc9a6160
Boby55 <halasi.gabor3@gmail.com>
2017-01-19 23:38:28 +0100

    Update hash_pass.py

--- Frisbee/Frisbee/hash_pass.py
+++ Frisbee/Frisbee/hash_pass.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
+import django
+try:
+        from django.contrib.auth.models import User
+        from django.contrib.auth.hashers import make_password
 
-users = User.objects.all()
+        users = User.objects.all()
 
-for user in users:
-	if 'pbkdf2_sha256' not in user.password:
-		user.password = (make_password(password=user.password,salt=None,hasher='pbkdf2_sha256'))
-		user.save()
+        for user in users:
+                if 'pbkdf2_sha256' not in user.password:
+                        user.password = (make_password(password=user.password,salt=None,hasher='pbkdf2_sha256'))
+                        user.save()
+except django.db.utils.ProgrammingError:
+        pass
